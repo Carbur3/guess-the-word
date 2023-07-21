7625ced5dd05599e63c25e02285407c0439de918
carolyn_b <burkecarolyn5@gmail.com>
2023-07-21 16:07:32 -0500

    Initial commit guess the word

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const guessedLettersElement = document.querySelector(".guessed-letters");
+const guessLetterButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProcess = document.querySelector(".word-in-progress");
+const remainingGuessesElement = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const 
